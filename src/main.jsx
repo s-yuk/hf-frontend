0dd05b9edffbcd041d31884cfa672e8910041bf5
shoq <117148816+Shoql@users.noreply.github.com>
2022-11-02 10:32:07 +0900

    日本語

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App, Login } from './_index.js'
+import { App, Login, Signup } from './_index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
