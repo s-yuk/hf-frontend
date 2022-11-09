ad8c2b98dc4256e9c9fb52c307da6f8b568bde68
Sho Tsutsui <59651963+BangFLugeL@users.noreply.github.com>
2022-11-09 11:59:53 +0900

    子供用アカウント会員情報ページ

--- src/pages/customer.jsx
+++ src/pages/customer.jsx
@@ -1,16 +1,72 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import * as React from 'react'
+import TextField from '@mui/material/TextField'
 import { Link } from 'react-router-dom'
 import {} from '../_index.js'
 import IconTabs from '../components/IconTabs_child.jsx'
+import '../css/customer.css'
+import { useState } from 'react'
 
 const Customer = () => {
+  const [name, setNmae] = useState('名前')
+  const [pass, setPass] = useState('パスワード')
+  const [newpass, setNewpass] = useState('newパスワード')
+  const [mail, setMail] = useState('メールアドレス')
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
   return (
     <>
       <header></header>
       <main></main>
       <div className='background2'>
-        <div className='btn-box'>
-          <div className='btn_customer'>
+        <div className='customerdata'>
+          <TextField
+            id='standard-multiline-flexible'
+            label='名前'
+            multiline
+            maxRows={4}
+            value={name}
+            onChange={(e) => handleChange(e)}
+            variant='standard'
+          />
+          <TextField
+            id='standard-multiline-flexible'
+            label='パスワード'
+            multiline
+            maxRows={4}
+            value={pass}
+            onChange={(e) => handleChange(e)}
+            variant='standard'
+          />
+          <TextField
+            id='standard-multiline-flexible'
+            label='新しいパスワード'
+            multiline
+            maxRows={4}
+            value={newpass}
+            onChange={(e) => handleChange(e)}
+            variant='standard'
+          />
+          <TextField
+            id='standard-multiline-flexible'
+            label='メールアドレス'
+            multiline
+            maxRows={4}
+            value={mail}
+            onChange={(e) => handleChange(e)}
+            variant='standard'
+          />
+          <div className='update-btn-box'>
+            <div className='update-btn'>
+              <Link to={`/`}>
+                <p>変更</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='logout-box'>
+          <div className='btn-customer'>
             <Link to={`/login`}>
               <p>ログアウト</p>
             </Link>
