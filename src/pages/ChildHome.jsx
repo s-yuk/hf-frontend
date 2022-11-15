d8692ec9b6587eab0a4e0f3483afb58e01cf4623
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-11-15 14:04:31 +0900

    make button component

---
+++ src/pages/ChildHome.jsx
@@ -0,0 +1,11 @@
+import { BigButton, MiddleButton, SmallButton } from '../components/Buttons'
+
+export const ChildHome = () => {
+  return (
+    <>
+      <BigButton text='交換' />
+      <MiddleButton text='戻る' />
+      <SmallButton text='履歴' />
+    </>
+  )
+}
