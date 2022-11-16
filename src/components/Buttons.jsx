22582311412d1c0fe419ef80bbcffe5e7d588071
Sho Tsutsui <59651963+BangFLugeL@users.noreply.github.com>
2022-11-16 12:00:44 +0900

    modalをいじった

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,4 +1,18 @@
-import { Button } from '@mui/material'
+import { Button, Modal, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { useState } from 'react'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
 
 export const SmallButton = ({ text }) => {
   return (
@@ -21,22 +35,49 @@ export const SmallButton = ({ text }) => {
 }
 
 export const MiddleButton = ({ text }) => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const click = () => {
+    console.log(1)
+    setOpen(true)
+  }
   return (
-    <Button
-      sx={{
-        width: '50%',
-        bgcolor: 'red',
-        borderRadius: '100px',
-        color: 'white',
-        fontSize: '1.2rem',
-        fontWeight: '600',
-        '&:hover': {
-          bgcolor: 'rgb(228, 0, 0)',
-        },
-      }}
-    >
-      {text}
-    </Button>
+    <Box>
+      <Button
+        sx={{
+          width: '50%',
+          bgcolor: 'red',
+          borderRadius: '100px',
+          color: 'white',
+          fontSize: '1.2rem',
+          fontWeight: '600',
+          '&:hover': {
+            bgcolor: 'rgb(228, 0, 0)',
+          },
+        }}
+        onClick={click}
+      >
+        {text}
+      </Button>
+
+      <Modal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <Typography id='modal-modal-title' variant='h6' component='h2'>
+            Text in a modal
+          </Typography>
+          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </Box>
   )
 }
 
