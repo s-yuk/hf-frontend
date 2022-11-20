e0825bac95708063293e4ce8f3077858aa8030d5
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-11-20 16:55:40 +0900

    子ども用メインページ

--- src/pages/ChildHome.jsx
+++ src/pages/ChildHome.jsx
@@ -1,10 +1,27 @@
 import { FolderSpecial } from '@mui/icons-material'
-import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material'
+import {
+  Modal,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from '@mui/material'
 import { Header } from '../components/Header'
 import { BigButton, SmallButton } from '../components/Buttons'
 import IconTabs from '../components/IconTabs'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const ChildHome = () => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => {
+    setOpen(true)
+  }
   return (
     <>
       <Header title='子ども用メインページ' />
@@ -19,8 +36,10 @@ const ChildHome = () => {
       <Box
         sx={{
           width: '90%',
-          maxHeight: 300,
+          height: 300,
           mx: 'auto',
+          mb: 3,
+          overflow: 'hidden',
         }}
       >
         <nav aria-label='main mailbox folders'>
@@ -36,35 +55,65 @@ const ChildHome = () => {
           </List>
         </nav>
         <Divider />
-
         <List
           sx={{
             width: '100%',
-            maxWidth: 360,
-            bgcolor: 'background.paper',
-            position: 'relative',
+            height: 300,
             overflow: 'auto',
-            maxHeight: 300,
             '& ul': { padding: 0 },
           }}
-          subheader={<li />}
         >
           {[0, 1, 2, 3, 4].map((sectionId) => (
-            <li key={`section-${sectionId}`}>
-              <ul>
-                <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-                {[0, 1, 2].map((item) => (
-                  <ListItem key={`item-${sectionId}-${item}`}>
-                    <ListItemText primary={`Item ${item}`} />
-                  </ListItem>
-                ))}
-              </ul>
-            </li>
+            <ul>
+              {[0, 1, 2].map((item) => (
+                <ListItem key={`item-${sectionId}-${item}`}>
+                  <ListItemText primary={`Item ${item}`} />
+                </ListItem>
+              ))}
+            </ul>
           ))}
         </List>
       </Box>
-      <BigButton text='交換' />
-      <SmallButton text='履歴' />
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 3,
+          mb: 1,
+        }}
+      >
+        <BigButton text='交換' handleOpen={handleOpen} />
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '60%',
+              bgcolor: 'background.paper',
+              border: '2px solid #000',
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <Typography id='modal-modal-title' variant='h6' component='h2'>
+              Text in a modal
+            </Typography>
+            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+        <SmallButton text='履歴' to='/child/history' />
+      </Box>
       <IconTabs />
     </>
   )
