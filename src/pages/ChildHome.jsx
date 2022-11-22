606385ad539c812ddcfa9ae0d51df3e1570e693f
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-11-22 23:06:48 +0900

    created child home

--- src/pages/ChildHome.jsx
+++ src/pages/ChildHome.jsx
@@ -8,20 +8,35 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  ListSubheader,
   Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from '@mui/material'
 import { Header } from '../components/Header'
-import { BigButton, SmallButton } from '../components/Buttons'
+import { BigButton, MiddleButton, SmallButton } from '../components/Buttons'
 import IconTabs from '../components/IconTabs'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { FixedSizeList } from 'react-window'
 
+function renderRow(props) {
+  const { index, style } = props
+
+  return (
+    <ListItem style={style} key={index} component='div' disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItemButton>
+    </ListItem>
+  )
+}
 const ChildHome = () => {
   const [open, setOpen] = useState(false)
   const handleOpen = () => {
     setOpen(true)
   }
+
   return (
     <>
       <Header title='子ども用メインページ' />
@@ -29,50 +44,65 @@ const ChildHome = () => {
         sx={{
           width: '90%',
           height: 150,
-          bgcolor: 'black',
+          bgcolor: 'green',
           mx: 'auto',
+          color: 'white',
+          borderRadius: '8px',
+          boxShadow: 3,
         }}
-      ></Box>
+      >
+        <Typography
+          variant='h6'
+          component='p'
+          sx={{
+            fontWeight: '600',
+            p: 2,
+          }}
+        >
+          現在のポイント
+        </Typography>
+        <Typography
+          variant='h3'
+          component='p'
+          sx={{
+            fontWeight: '700',
+            textAlign: 'right',
+            p: 2,
+            '&::after': {
+              content: '"pt"',
+              fontSize: '1.5rem',
+            },
+          }}
+        >
+          {500}
+        </Typography>
+      </Box>
+      <nav aria-label='main mailbox folders'>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <FolderSpecial
+                sx={{
+                  mr: 2,
+                }}
+              />
+              <ListItemText primary='交換先一覧' />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
       <Box
         sx={{
           width: '90%',
           height: 300,
           mx: 'auto',
-          mb: 3,
-          overflow: 'hidden',
+          bgcolor: 'background.paper',
         }}
       >
-        <nav aria-label='main mailbox folders'>
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <FolderSpecial />
-                </ListItemIcon>
-                <ListItemText primary='交換先一覧' />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </nav>
-        <Divider />
-        <List
-          sx={{
-            width: '100%',
-            height: 300,
-            overflow: 'auto',
-            '& ul': { padding: 0 },
-          }}
-        >
-          {[0, 1, 2, 3, 4].map((sectionId) => (
-            <ul>
-              {[0, 1, 2].map((item) => (
-                <ListItem key={`item-${sectionId}-${item}`}>
-                  <ListItemText primary={`Item ${item}`} />
-                </ListItem>
-              ))}
-            </ul>
-          ))}
-        </List>
+        <FixedSizeList height={300} width={360} itemSize={46} itemCount={30} overscanCount={5}>
+          {renderRow}
+        </FixedSizeList>
       </Box>
       <Box
         sx={{
@@ -97,19 +127,46 @@ const ChildHome = () => {
               top: '50%',
               left: '50%',
               transform: 'translate(-50%, -50%)',
-              width: '60%',
+              width: '65%',
+              height: '30%',
               bgcolor: 'background.paper',
               border: '2px solid #000',
               boxShadow: 24,
               p: 4,
             }}
           >
-            <Typography id='modal-modal-title' variant='h6' component='h2'>
-              Text in a modal
+            <Typography
+              id='modal-modal-title'
+              variant='h6'
+              component='h2'
+              sx={{
+                textAlign: 'center',
+                mb: 2,
+              }}
+            >
+              何個交換しますか?
             </Typography>
-            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <Box>
+              <FormControl fullWidth>
+                <InputLabel id='demo-simple-select-label'>個数</InputLabel>
+                <Select sx={{}} labelId='demo-simple-select-label' id='demo-simple-select' label='count'>
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                </Select>
+                <Box
+                  sx={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    mt: 3,
+                  }}
+                >
+                  <MiddleButton text='交換' />
+                </Box>
+              </FormControl>
+            </Box>
           </Box>
         </Modal>
         <SmallButton text='履歴' to='/child/history' />
